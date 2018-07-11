1fd4979a0b220c7168cac0293405567c5d9222b1
Tristan Brown <tgbrown@stanford.edu>
2018-07-11 07:15:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function nestedTarget() {
  function increaseRankBy(n)
   var lis = document.querySelectorAll('ul.ranked-list li');
   for (var i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+    lis[i].innerHTML = parseInt(ranks[i].innerHTML) + n
   }
